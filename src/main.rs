66fd266a985434a8f919875c19db4bd4dc2555b1
Anderson Acuña <andersonyacvpers002360@outlook.com>
2024-06-13 20:43:06 -0500

    Agregada función para imprimir matrices 3x3.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+    matrix_printer(&matrix);
+}
+
+fn matrix_printer(matrix: &[[i8; 3]; 3]) {
+    println!("[");
+    for row in matrix {
+        println!("[");
+        for column in row {
+            println!("{column}");
+        }
+        println!("]");
+    }
+    println!("]");
 }
